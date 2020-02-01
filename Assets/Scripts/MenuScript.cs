e4b7a44927e9a6d874ea105a6f971413f6019738
BlankRip <aniketh_g5@outlook.com>
2020-02-01 10:26:56 +0400

    Maybe final menu

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -9,19 +9,22 @@ public class MenuScript : MonoBehaviour
      [SerializeField] GameObject titleObject;
      [SerializeField] GameObject playButtonObject;
      [SerializeField] GameObject infoObject;
+     [SerializeField] Animation titleAnimation;
      [SerializeField] Animation solidColorAnim;
 
     public void PlayClicked()
     {
-        titleObject.SetActive(false);
-        infoObject.SetActive(true);
-        solidColorAnim.Play();
+        titleAnimation.Play();
         playButtonObject.SetActive(false);
         StartCoroutine(MoveToGame());
     }
 
     IEnumerator MoveToGame()
     {
+        yield return new WaitForSeconds(2.6f);
+        titleObject.SetActive(false);
+        infoObject.SetActive(true);
+        solidColorAnim.Play();
         yield return new WaitForSeconds(5f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
